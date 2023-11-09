ee8e84689f2b26a6817ff1000d93bcaa385af4de
Shedhika S Maliyekkal <barbieshedoll1@gmail.com>
2023-11-09 10:52:11 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    present = db.Column(db.Boolean, default=False)
+
+@app.route('/')
+def index():
+    students = Student.query.all()
+    return render_template('index.html', students=students)
+
+@app.route('/toggle/<int:id>')
+def toggle(id):
+    student = Student.query.get(id)
+    student.present = not student.present
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
